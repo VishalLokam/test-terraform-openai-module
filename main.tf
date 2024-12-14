0b0b0f1681ee3b7caed6434abfdcb5a5b167d99d
VishalLokam <vishalvikaslokam@gmail.com>
2024-12-14 11:49:32 +0530

    Variable for dynamic throttling

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "azureopenai_rg1" {
+resource "azurerm_resource_group" "azureopenai_rg" {
   name     = "azureopenai_rg1"
   location = var.location
 }
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "azureopenai_rg1" {
 # public_network_access_enabled set to false to restrict access from the internet.
 # outbound_network_access_restricted set to true to deny access exceptions from trusted services.
 # custom_subdomain_name required for private endpoint.
-resource "azurerm_cognitive_account" "azureopenai_cognitive_account1" {
-  resource_group_name        = azurerm_resource_group.azureopenai_rg1.name
+resource "azurerm_cognitive_account" "azureopenai_cognitive_account" {
+  resource_group_name        = azurerm_resource_group.azureopenai_rg.name
   name                       = "azureopenai_cognitive_account"
   kind                       = "OpenAI"
   location                   = var.location
@@ -20,7 +20,7 @@ resource "azurerm_cognitive_account" "azureopenai_cognitive_account1" {
 # sku is standard with 50k token per minute(TPM) limit. 1 capacity = 1k TPM
 resource "azurerm_cognitive_deployment" "azureopenai_cognitive_deployment" {
   name                       = var.deployment_name
-  cognitive_account_id       = azurerm_cognitive_account.azureopenai_cognitive_account1.id
+  cognitive_account_id       = azurerm_cognitive_account.azureopenai_cognitive_account.id
   dynamic_throttling_enabled = var.dynamic_throttling_enabled
   model {
     format = "OpenAI"
